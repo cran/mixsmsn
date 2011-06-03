71ce853f19306f3bd99d4ef5d3dfb65e57354f64
Marcos Prates <marcosop@est.ufmg.br>
2011-06-03 00:00:00 +0000

    version 1.0-5

--- R/mix.dens.R
+++ R/mix.dens.R
@@ -49,3 +49,47 @@ mix.dens <- function(y, model, log=FALSE, ylab=NULL, xlab = NULL, main = NULL, .
 	}
      }
 }
+##################################################################
+##########     Lines	                     #####################
+
+mix.lines <- function(y, model, log=FALSE, ...){
+  if((class(model) != "t") && (class(model) != "Skew.t") && (class(model) != "Skew.cn") && (class(model) != "Skew.slash") && (class(model) != "Skew.normal") && (class(model) != "Normal")) stop(paste("Class of family",class(model),"not recognized.",sep=" "))
+  y <- as.matrix(y) 
+  if (dim(y)[2] != 1) stop("The mix.dens function is only appropriate for the univariate analysis.\n")
+
+     lim <- -9
+     xx=seq(min(y),max(y),(max(y)-min(y))/1000)   
+
+     if ((class(model) == "Skew.t") || class(model) == "t"){
+      if(!log) lines(xx,d.mixedST(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu), ...)
+      else{
+         aux <- log(d.mixedST(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu))
+	 aux[which(aux < lim)] <- NA
+         lines(xx,aux,...)
+	}
+     }
+     if (class(model) == "Skew.cn"){
+      if(!log) lines(xx,d.mixedSNC(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu),...)
+      else{
+	 aux <- log(d.mixedSNC(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu))
+	 aux[which(aux < lim)] <- NA
+         lines(xx,aux,...)
+	}
+     }
+     if (class(model) == "Skew.slash"){
+      if(!log) lines(xx,d.mixedSS(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu),...)
+      else{
+	 aux <- log(d.mixedSS(xx, model$pii, model$mu, model$sigma2, model$shape, model$nu))
+	 aux[which(aux < lim)] <- NA
+         lines(xx,aux,...)
+	}
+     }
+     if ((class(model) == "Skew.normal") || (class(model) == "Normal")){
+      if(!log) lines(xx,d.mixedSN(xx, model$pii, model$mu, model$sigma2, model$shape),...)
+      else{
+	 aux <- log(d.mixedSN(xx, model$pii, model$mu, model$sigma2, model$shape))
+	 aux[which(aux < lim)] <- NA
+         lines(xx,aux,...)
+	}
+     }
+}
